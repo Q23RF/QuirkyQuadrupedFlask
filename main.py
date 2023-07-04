b723b68cb4819fa72edc5db8625ea109e268c9d1
Q23RF <97279294+Q23RF@users.noreply.github.com>
2023-07-04 02:31:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+from mastodon import Mastodon
+import random
+from pytube import Playlist
+
+app = Flask(__name__)
+mastodon = Mastodon(access_token=os.environ['TOKEN'],
+                    api_base_url='https://botsin.space/')
+
+
+def get_random_vid(playlist_url):
+    p = Playlist(playlist_url)
+    urls = p.video_urls
+    url = urls[random.randint(0, len(urls) - 1)]
+    return url
+
+mspl = "https://www.youtube.com/playlist?list=OLAK5uy_mhIfeEQekE1BSH2Qzwj-3AU-wTzIiC2Q4"
+
+
+@app.route('/', methods=['HEAD', 'GET'])
+def index():
+    ua = request.headers.get('User-Agent')
+    if ua == 'pipedream/1':
+        url = get_random_vid(mspl)
+        txt = "#推歌 #每日梯曲 \n"
+        mastodon.status_post(txt + url)
+    return 'Hello!'
+
+
+app.run(host='0.0.0.0', port=81)
